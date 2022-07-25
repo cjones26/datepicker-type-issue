30290dc26b7ba7478642abd63a2e26e77fbb1820
Charles Jones <charlie.jones@varomoney.com>
2022-07-25 12:25:03 -0400

    Initial commit.

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material/styles';
+import type {} from '@mui/x-date-pickers/themeAugmentation';
+
+export const theme = {
+  components: {},
+};
+
+const createdTheme = createTheme(theme);
+
+export default createdTheme;
